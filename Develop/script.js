11b9e6dec339c246c3d58618d6f19e10726ef37d
Omari Grampus <ojgrampus@gmail.com>
2021-03-06 09:57:51 -0500

    added for loop, and correct happy path

--- Develop/script.js
+++ Develop/script.js
@@ -10,66 +10,80 @@ function writePassword() {
 
 }
 
-//Dom Elements
-const resultEL = document.getElementById('result')
-const lengthEl = document.getElementById('length')
-const uppercaseEl = document.getElementById('uppercase')
-const lowercaseEl = document.getElementById('lowercase')
-const numbersEl = document.getElementById('numbers')
-const symbolsEl = document.getElementById('symbols')
-const generateEl = document.getElementById('generate')
-const clipboardEl = document.getElementById('clipboard')
+// Add event listener to generate button            
+generateBtn.addEventListener("click", writePassword);
+
+const passlength = ''
+const captials = "ABCDEFGHIJKLMNOPQRSTUVWXYZx"
+const lowers = "abcdefghijklmnopqrstuvwxyz";
+const specials = "!#$%&@*";
+const number = "123456789";
 
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", () => {
-  const length = lengthEl.value
-  console.log(length)
-}
-
-const captials = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const lowers = "abcdefghijklmnopqrstuvwxyz"
-const specials = "!#$%&@*"
-const number = "123456789"
 
 // Make GeneratePassword function Prompts
 function generatePassword() {
-  var Passwordlength = window.prompt("Please choose between 8 and 128 characters?");
-  var Lowercase = window.prompt("Would you like lowercase letters, yes or no?");
-  var Uppercase = window.prompt("Would you like uppcase characters, yes or no?");  
-  var Numeric = window.prompt ("Would you like to included number from 1 and 9, yes or no");
-  var SpecialCharcters = window.prompt("Would you like special characters, yes or no");
-}
+  
+  var finalPassword = "";
+  var allowedChars = "";
+  
+  
+  var Passwordlength = window.prompt(        
+    "How many characters would you like. Please choose between 8 and 128 characters?");
+    // only want to pass in a number
+    // check out isNAN
+    console.log(Passwordlength);
+    if (Passwordlength < 8 || Passwordlength > 128)
+    window.prompt ("please choose correct amount of characters")
+  
+    
+  
+    var Lowercase = confirm("Would you like lowercase letters, yes or no?");
+    console.log(Lowercase);
+    if (Lowercase) {
+      allowedChars += lowers;
+    }
+
+    var Uppercase = window.confirm("Would you like uppcase characters, yes or no?");
+    console.log(Uppercase);
+    if (Uppercase) {
+      allowedChars += captials
+    }  
+  
+    var Numeric = window.confirm("Would you like to included a numbers, yes or no?");
+    console.log(Numeric);
+    if (Numeric) {
+      allowedChars += number
+    }
+  
+    var SpecialCharacters = window.confirm("Would you like special characters, yes or no?");
+    console.log(SpecialCharacters);
+    if (SpecialCharacters) {
+      allowedChars += specials
+
+    }
+      console.log(allowedChars)
+    // need all letters from user options to go into allowedchars
+
+    
 
 
 
-var randomFunctions = {
-  lower: getRandomlower,
-  upper: getRandomupper,
-  number: getRandomnumber,
-  symbol: Specialcharacter
-}
 
 
 
-//Generate Lowercase Function
-function getRandomlower() {
-  return String.fromCharCode(math.floor(math.random() * 26) + 97)
-}
 
-//Generate Uppercase Function
-function getRandomupper() {
-  return String.fromCharCode(math.floor(math.random() * 26) + 65)
-}
+     for (let i = 0; i < Passwordlength; i++) {
+      const randomNumber = (Math.floor(Math.random() * allowedChars.length ));
+      const randomLetter = allowedChars[randomNumber]
+    console.log(randomLetter)
+      
+    finalPassword += randomLetter
 
-//Generate Numeric Function
-function getRandomnumber() {
-  return String.fromCharCode(math.floor(math.random() * 10) + 48)
-}
+    }
 
-//Generate Special Character Function
-function Specialcharacter() {
-var Specialcharacters = "!#$%&@*";
-return Specialcharacters [Math.floor(Math.random() * SpecialCharcters.length)];
-}
+    
+
+      
+    return finalPassword;
+  }
